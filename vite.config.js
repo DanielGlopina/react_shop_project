16bdc4b8ddd98bb46b2c63ed77420bcad0129ebd
Daniel Hlopina <glopinadaniel@gmail.com>
2025-06-15 16:51:12 +0300

    Version 1.0.0

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: "/react_shop_project/",
+
+export default defineConfig(({ mode }) => ({
+  base: mode === "production" ? "/react_shop_project/" : "/",
   plugins: [react()],
-});
+}));
